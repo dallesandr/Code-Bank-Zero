e456c57cb31c56c4c17bcd8e78cdb300f0a3e193
David Allesandro <dallesandr@gmti.gannett.com>
2015-03-12 15:29:17 -0400

    2028Digicert Netscaler

---
+++ lbsslv2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#USE: ./lbsslv2.sh <path to crt files>
+#This pulls in all the names of the crt files at the path and creates netscaler certkey configs.
+#Example: ./lbsslv2.sh /home/username/SSL
+
+/bin/ls -1 $1/*.crt | sed -e "s/.crt$//" |  sed -e "s/^.*\///" | while read SITE
+
+do
+	echo add ssl certKey $SITE -cert \"/nsconfig/ssl/sites/$SITE.crt\" -key \"/nsconfig/ssl/sites/$SITE.key\" -expiryMonitor ENABLED
+	echo link ssl certKey $SITE DigiCertCA2028
+done
+
