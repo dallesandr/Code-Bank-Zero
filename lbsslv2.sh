b33cf8bec0cf51d1eff7ff8838a53d633b26fb72
David Allesandro <dallesandr@gmti.gannett.com>
2015-03-12 15:37:04 -0400

    header Update

--- lbsslv2.sh
+++ lbsslv2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #USE: ./lbsslv2.sh <path to crt files>
 #This pulls in all the names of the crt files at the path and creates netscaler certkey configs.
-#Example: ./lbsslv2.sh /home/username/SSL
+#Example: ./lbsslv2.sh /home/username/SSL/. or ./lbsslv2.sh folder/.
+#
+#version:2.0
+#date   :20150312
 
 /bin/ls -1 $1/*.crt | sed -e "s/.crt$//" |  sed -e "s/^.*\///" | while read SITE
 
